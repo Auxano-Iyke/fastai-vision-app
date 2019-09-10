ab81e05947fca6fa5ec298a39b5a676cf7fa404d
Onwukwe Ikechukwu <onwukwe.ikechukwu@outlook.com>
2019-09-10 13:57:53 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -7,8 +7,8 @@ from io import BytesIO
 from fastai.vision import *
 import base64
 
-model_file_url = 'https://www.googleapis.com/drive/v3/files/1ieMIQx7L6EfeK9YujoUtYU2tKUv1ST5y?alt=media&key=AIzaSyBt4CGECZtVUZMHj3Eb4Cp_RGYwd7_qgxw'
-model_file_name = 'model.pth'
+model_file_url = ''https://www.googleapis.com/drive/v3/files/1RzUOojU_Lc-BeFHPJiEaiKB7BdrJf-WN?alt=media&key=AIzaSyBt4CGECZtVUZMHj3Eb4Cp_RGYwd7_qgxw'
+model_file_name = 'fastai_resnet.pkl'
 classes = ['AM General Hummer SUV 2000',
  'Acura Integra Type R 2001',
  'Acura RL Sedan 2012',
