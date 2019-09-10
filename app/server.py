8cf9719ed3daa1942a4fb5e51cd651e7fa1a3862
Onwukwe Ikechukwu <onwukwe.ikechukwu@outlook.com>
2019-09-10 13:42:46 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -219,9 +219,9 @@ async def download_file(url, dest):
             with open(dest, 'wb') as f: f.write(data)
             
 async def setup_learner():
-    await download_file(export_file_url, path / export_file_name)
+    await download_file(model_file_url, path / model_file_name)
     try:
-        learn = load_learner(path, export_file_name)
+        learn = load_learner(path, model_file_name)
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
